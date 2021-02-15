cdc37c0ef9d7693f9a1c93b38bda2471827462c8
arbid2544 <mrarbid.m@ku.th>
2021-02-15 16:59:56 +0700

    Update 111_lab3-1.c

--- 111_lab3-1.c
+++ 111_lab3-1.c
@@ -1,6 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int a;
+    int a[4],b[4],c[4],d[4],e[4],i;
+    for(i=0;i<4;i++){
+        scanf("%d",&a[i]);
+        printf("%d",a[i]);
+        }
+    for(i=0;i<4;i++){
+        scanf("%d",&b[i]);
+        printf("%d",b[i]);
+        }
+    for(i=0;i<4;i++){
+        scanf("%d",&c[i]);
+        printf("%d",c[i]);
+        }
+    for(i=0;i<4;i++){
+        scanf("%d",&d[i]);
+        printf("%d",d[i]);
+        }
+    for(i=0;i<4;i++){
+        scanf("%d",&e[i]);
+        printf("%d",e[i]);
+        }
+
     return 0;
 }
