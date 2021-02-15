3835e17ab1389f00a14c574a171449d5c13637a0
arbid2544 <mrarbid.m@ku.th>
2021-02-15 19:04:51 +0700

    Update 111_lab3-2.c

--- 111_lab3-2.c
+++ 111_lab3-2.c
@@ -1,15 +1,14 @@
 #include<stdio.h>
 int main()
 {
-    int i,x;
+    int i,x,prime,num,sum;
     scanf("%d",&x);
-    int prime[x],num[x],sum[x];
     for(i=0;i<x;i++)
     {
-        scanf("%d",&prime[i]);
-        scanf("%d",&num[i]);
-        sum[i]+=num[i];
+        scanf("%d",&prime);
+        scanf("%d",&num);
+        sum+=num;
     }
-    printf("%d %d",i,sum[1]);
+    printf("%d %d",i,sum);
     return 0;
 }
