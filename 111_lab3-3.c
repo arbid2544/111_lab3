5a672b896737954806e3e2c88b697996d6235888
arbid2544 <mrarbid.m@ku.th>
2021-02-15 17:47:35 +0700

    Create 111_lab3-3.c

---
+++ 111_lab3-3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num,i;
+    scanf("%d",&num);
+    if(num==0)
+        printf("1\n");
+    for(i=2;;i++)
+    {
+        if(num/i==0)
+        {
+        }
+    }
+}
