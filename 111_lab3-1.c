f750c2d34db67cacd1963f43df0971b8f4393e5b
arbid2544 <mrarbid.m@ku.th>
2021-02-15 17:15:41 +0700

    Update 111_lab3-1.c

--- 111_lab3-1.c
+++ 111_lab3-1.c
@@ -1,28 +1,36 @@
 #include<stdio.h>
 int main()
 {
-    int a[4],b[4],c[4],d[4],e[4],i,s1=0,s2=0,s3=0,s4=0,s5=0;
+    int a[4],b[4],c[4],d[4],e[4],i,s[5]={0},max=0,p;
     for(i=0;i<4;i++){
         scanf("%d",&a[i]);
-        s1+=a[i];
+        s[0]+=a[i];
         }
     for(i=0;i<4;i++){
         scanf("%d",&b[i]);
-        s2+=b[i];
+        s[1]+=b[i];
         }
     for(i=0;i<4;i++){
         scanf("%d",&c[i]);
-        s3+=c[i];
+        s[2]+=c[i];
         }
     for(i=0;i<4;i++){
         scanf("%d",&d[i]);
-        s4+=d[i];
+        s[3]+=d[i];
         }
     for(i=0;i<4;i++){
         scanf("%d",&e[i]);
-        s5+=e[i];
+        s[4]+=e[i];
         }
-    printf("%d %d %d %d %d",s1,s2,s3,s4,s5);
+    for(i=0;i<4;i++)
+    {
+        for(i=0;i<4;i++){
+           if(max<s[i]){
+              max=s[i];
+              p=i+1;}
+        }
+    }
+    printf("%d %d",p,max);
 
     return 0;
 }
