bfe3a58207a5ab2f8eade11770e9f8f1dd1098fd
arbid2544 <mrarbid.m@ku.th>
2021-02-15 19:35:31 +0700

    Update 111_lab3-5.c

--- 111_lab3-5.c
+++ 111_lab3-5.c
@@ -1,29 +1,7 @@
 #include<stdio.h>
 int main()
 {
-    /*long num,i,j=1,ber,x=0;
-    scanf("%d",&num);
-    ber=num;
-    int a[num];
-     for(i=2;num>1;i++,j++)
-    {
-        if(num%i==0)
-        {
-            a[j]=i;
-            if(a[j]==a[j-1])
-                {
-                   printf("%d is not a Lukky Number.",ber);
-                   x=1;
-                   break;
-                }
-            num/=i;
-            i=1;
-            x+=1;
-        }
-    }
-    if(x==3)
-        printf("%d is a Lukky Number.",ber);*/
-    int num,i,j=1,ber,x=0;
+    long num,i,j=1,ber,x=0;
     scanf("%d",&num);
     ber=num;
     int a[num];
@@ -32,7 +10,6 @@ int main()
     {
         if(num%i==0)
         {
-            printf("%d\n",i);
             a[j]=i;
             if(a[j]==a[j-1])
                 x=100;
