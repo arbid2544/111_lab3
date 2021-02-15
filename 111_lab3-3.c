f216dd4088a0a46a43e7cbf617cd588581176770
arbid2544 <mrarbid.m@ku.th>
2021-02-15 17:57:12 +0700

    Update 111_lab3-3.c

--- 111_lab3-3.c
+++ 111_lab3-3.c
@@ -3,12 +3,17 @@ int main()
 {
     int num,i;
     scanf("%d",&num);
-    if(num==0)
+    if(num==1)
         printf("1\n");
-    for(i=2;;i++)
+    for(i=2;num>1;i++)
     {
-        if(num/i==0)
+        if(num%i==0)
         {
+            printf("%d\n",i);
+            num/=i;
+            i=1;
         }
     }
+    printf("0");
+    return 0;
 }
